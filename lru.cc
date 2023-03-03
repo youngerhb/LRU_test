beeacd52ab76397baa8993f10b4631013a6bb0e8
youngerhb <2540586089@qq.com>
2023-03-03 17:54:57 +0800

    加了个打印所有节点的函数,还有完善了前面的函数，还差测试和makefile没写

--- lru.cc
+++ lru.cc
@@ -1,48 +1,72 @@
-#include<iostream>
+#include <iostream>
 
 class lru_list
 {
 private:
     /* data */
-    struct ListNode {
-    int key;
-    int value;        
-    ListNode *pre,*next;
-    ListNode(int k,int v) : key(k), value(v), pre(nullptr), next(nullptr) {};
-    ListNode(int k, int v,ListNode *pre_node, ListNode*next_node) : key(k),value(v), pre(pre_node), next(next_node) {};
+    struct ListNode
+    {
+        int key;
+        int value;
+        ListNode *pre, *next;
+        ListNode(int k, int v) : key(k), value(v), pre(nullptr), next(nullptr){};
+        ListNode(int k, int v, ListNode *pre_node, ListNode *next_node) : key(k), value(v), pre(pre_node), next(next_node){};
     };
 
     ListNode *head = nullptr;
-public:
-    
-    void add_node(int k, int v){
-        head = new ListNode(k,v,nullptr,head);
-    }
 
-    int check_value(int k){
-        ListNode* node = head;
-        while (node!=nullptr)
+public:
+    void add_node(int k, int v)
+    {
+        head = new ListNode(k, v, nullptr, head);
+    }
+    
+    int check_value(int k)
+    {
+        ListNode *node = head;
+        while (node != nullptr)
         {
             /* code */
-            if(node->value==k){
-                std::cout<<"the value of "<< k << "is" << node->value <<std::endl;
+            if (node->value == k)
+            {
+                std::cout << "the value of " << k << "is" << node->value << std::endl;
+
+                if(node->pre!=nullptr){
+                    node->pre->next = node->next; 
+                    
+                    if(node->next!=nullptr){
+                    node->next->pre = node->pre;
+
+                    node->next=node->pre;
+
+                    if(node->pre->pre!=nullptr){
+                        node->pre->pre->next = node;
+                    }
+                    node->pre = node->pre->pre;                  
+                }
+                }
+                
                 return node->value;
             }
             node = node->next;
         }
-        
-        std::cout << "the key is not in the list" <<std::endl;
+
+        std::cout << "the key is not in the list" << std::endl;
         return -1;
     }
 
-    bool delete_node(int k){
-        ListNode* node = head;
-        while (node!=nullptr)
+    bool delete_node(int k)
+    {
+        ListNode *node = head;
+        while (node != nullptr)
         {
             /* code */
-            if(node->value==k){
-                std::cout<<"the value of " << k <<"is" << node->value <<std::endl;
+            if (node->key == k)
+            {
+                std::cout << "the value of " << k << "is" << node->value << std::endl;
+                if(node->pre!=nullptr)
                 node->pre->next = node->next;
+                if(node->next!=nullptr)
                 node->next->pre = node->pre;
                 delete node;
                 return 1;
@@ -51,8 +75,14 @@ public:
         }
         return 0;
     }
+
+    void print_all_nodes(){
+        ListNode* node = head;
+        while ((node!=nullptr))
+        {
+            /* code */
+            std::cout<<"key is : " << node->key << " value is : "<< node->value<<std::endl;
+            node = node->next;
+        }        
+    }
 };
-
-
-
-
